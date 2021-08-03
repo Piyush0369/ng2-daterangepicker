bc03496c57577da9898c9dfb49cbf54f527456c4
Piyush Dewangan <piyush0369@gmail.com>
2021-08-03 15:30:11 +0530

    Custom Ranges Added

--- projects/ng2-daterangepicker/src/lib/ng2-daterangepicker.service.ts
+++ projects/ng2-daterangepicker/src/lib/ng2-daterangepicker.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,20 @@ export class DaterangepickerConfig {
   public settings: any;
 
   constructor() {
-    this.settings = {};
+    this.settings = {
+      locale: { format: 'DD-MM-YYYY' },
+      alwaysShowCalendars: false,
+      opens: "right",
+      ranges: {
+        'Last 7 Days': [moment().subtract(1, 'week'), moment()],
+        'Last 14 Days': [moment().subtract(2, 'week'), moment()],
+        'Last 30 Days': [moment().subtract(30, 'day'), moment()],
+        'Last 3 Months': [moment().subtract(4, 'month'), moment()],
+        'Last 6 Months': [moment().subtract(6, 'month'), moment()],
+        'Last 12 Months': [moment().subtract(12, 'month'), moment()],
+        'Quater to Date': [moment().quarter(moment().quarter()).startOf('quarter'), moment()],
+        'Month to Date': [moment().month(moment().month()).startOf('month'), moment()],
+      }
+    };
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import moment from 'moment';
-import { DaterangepickerConfig } from "ng2-daterangepicker-plus";
+import { DaterangepickerConfig } from "../../projects/ng2-daterangepicker/src/public-api";
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,15 @@ export class AppComponent {
   title = 'angular9';
 
   public chosenDate: any = {
-    start: moment().subtract(12, 'month'),
-    end: moment().subtract(6, 'month'),
+    start: moment("2021-02-01"),
+    end: moment("2021-05-01"),
+    label: "Custom Range"
   };
 
-  public picker1 = {
-    opens: 'left',
-    startDate: moment().subtract(5, 'day'),
-    endDate: moment(),
+  public options = {
+    opens: 'right',
+    startDate: this.chosenDate.start,
+    endDate: this.chosenDate.end,
     isInvalidDate: function (date: any) {
       if (date.isSame('2017-09-26', 'day'))
         return 'mystyle';
@@ -31,7 +32,9 @@ export class AppComponent {
 
   public singleDatepicker = {
     singleDatePicker: true,
-    autoPosition: true
+    opens: 'center',
+    autoPosition: true,
+    ranges: {}
   }
 
   public singleDate: any = {
@@ -39,21 +42,25 @@ export class AppComponent {
   };
 
   constructor(private daterangepickerOptions: DaterangepickerConfig) {
-    this.daterangepickerOptions.settings = {
-      locale: { format: 'YYYY-MM-DD' },
-      alwaysShowCalendars: false,
-      "opens": "right",
-      ranges: {
-        'Last Month': [moment().subtract(1, 'month'), moment()],
-        'Last 3 Months': [moment().subtract(4, 'month'), moment()],
-        'Last 6 Months': [moment().subtract(6, 'month'), moment()],
-        'Last 12 Months': [moment().subtract(12, 'month'), moment()],
-      }
-    };
+    // this.daterangepickerOptions.settings = {
+    //   locale: { format: 'YYYY-MM-DD' },
+    //   alwaysShowCalendars: false,
+    //   "opens": "right",
+    //   ranges: {
+    //     'Last Month': [moment().subtract(1, 'month'), moment()],
+    //     'Last 3 Months': [moment().subtract(4, 'month'), moment()],
+    //     'Last 6 Months': [moment().subtract(6, 'month'), moment()],
+    //     'Last 12 Months': [moment().subtract(12, 'month'), moment()],
+    //     'Last 4 Months': [moment().subtract(5, 'month'), moment()],
+    //   }
+    // };
+
+    console.log(this.chosenDate)
   }
 
   public selectedDate(value: any, dateInput: any): void {
     console.log(value);
+    console.log(dateInput)
     dateInput.start = value.start;
     dateInput.end = value.end;
   }
@@ -69,10 +76,11 @@ export class AppComponent {
   public updateSettings(): void {
     this.daterangepickerOptions.settings.locale = { format: 'YYYY/MM/DD' };
     this.daterangepickerOptions.settings.ranges = {
+      'Last 7 Days': [moment().subtract(2, 'week'), moment()],
       '30 days ago': [moment().subtract(1, 'month'), moment()],
       '3 months ago': [moment().subtract(4, 'month'), moment()],
       '6 months ago': [moment().subtract(6, 'month'), moment()],
-      '7 months ago': [moment().subtract(12, 'month'), moment()],
+      '12 months ago': [moment().subtract(12, 'month'), moment()],
     };
   }
 }
